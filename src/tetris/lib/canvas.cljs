d3aef78a251738986c6c2e95a0687a0985d264c1
Stuart Campbell <stuart@harto.org>
2015-07-27 09:44:34 -0700

    Import more canvas functions

--- src/tetris/lib/canvas.cljs
+++ src/tetris/lib/canvas.cljs
@@ -12,3 +12,12 @@
   ([ctx x y w h]
    (.clearRect ctx x y w h)
    ctx))
+
+(defn set-properties! [ctx props]
+  (doseq [[k v] props]
+    (aset ctx k v))
+  ctx)
+
+(defn fill-text! [ctx x y s]
+  (.fillText ctx s x y)
+  ctx)
